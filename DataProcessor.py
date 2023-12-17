aa9a1cc489849b566be52fa5b37a0ec435383406
PairOfPants <adenham112@gmail.com>
2023-12-17 16:23:14 -0500

    Added Base Objects
    Each player will have their own object with corresponding member variables. A notable property of this class is its aggregation relationship with the Statistics class, as each player will have a list of Statistic objects. Once the data is scraped online each player will have their data loaded into their respective object for me to manipulate to the user's choosing

---
+++ DataProcessor.py
@@ -0,0 +1,39 @@
+def printData(playerData, playerName):
+    print(playerName + '\'s data:')
+    for i in range(len(playerData)):
+        print(playerData[i])
+
+def innerProduct(v1, v2):
+    dotProduct = 0.0
+    if(len(v1) != len(v2)):
+        print("VECTORS ARE NOT IN THE SAME DIMENSION SUBSPACE")
+    else:
+        for i in range(len(v1)):
+            dotProduct = dotProduct+(v1[i]*v2[i])
+    return dotProduct
+
+def normOf(v):
+    norm = 0.0
+    for i in range(len(v)):
+        norm += v[i]**2
+    norm = norm**0.5
+    return norm
+
+def multiplyScalarToVector(s, v):
+    ret = v
+    for i in range(len(v)):
+        ret[i] = v[i]*s
+    return ret
+
+ljData = [2.96,1.28]
+shirtData = [5.71,1.49]
+playerName = 'aidan'
+printData(ljData,playerName)
+
+dotProd = innerProduct(ljData, shirtData)
+normSquared = normOf(shirtData)**2
+projectedVector = multiplyScalarToVector((dotProd/normSquared),shirtData)
+print("DOT PRODUCT: ",dotProd)
+print("NORM SQUARED: ",normSquared)
+print(projectedVector)
+
---
+++ venv/Driver.py
@@ -0,0 +1,45 @@
+class Statistic:
+    def __init_(self):
+        self.name = "NO_NAME"
+        self.value = 0
+    def __init__(self,name,value):
+        self.name = name
+        self.value = value
+    def __str__(self):
+        return f"{self.name}: {self.value}"
+    
+class Player:
+    def __init__ (self):
+        self.fname = "FNAME"
+        self.lname = "LNAME"
+        self.number = 00
+        self.gradYear = 0000
+        self.isPitcher = False
+        self.isHitter = False
+        self.statistics = []
+    def __init__(self, fname, lname, number, gradYear, isPitcher, isHitter):
+        self.fname = fname
+        self.lname = lname
+        self.number = number
+        self.gradYear = gradYear
+        self.isPitcher = isPitcher
+        self.isHitter = isHitter
+        self.statistics = []
+    def addStatistic(self,stat):
+        self.statistics.append(stat)
+    def printPerson(self):
+        print("************")
+        print("PLAYER: ", self.fname, " ", self.lname)
+        print("#",self.number, "\tGraduation Year: ",self.gradYear)
+        if(self.isPitcher and not self.isHitter):
+            print("Pitcher Only")
+        elif(not self.isPitcher and self.isHitter):
+            print("Position Player")
+        elif(self.isHitter and self.isPitcher):
+            print("2-Way Player")
+        print()
+        print("STATISTICS")
+        for i in range(len(self.statistics)):
+            print(self.statistics[i])
+        print("************")
+
